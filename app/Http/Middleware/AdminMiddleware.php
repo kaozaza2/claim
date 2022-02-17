a8e1fe4d92d6b441b41454c9cf2151aabec4703b
Kao Kittisak <kittisakphiri@gmail.com>
2022-02-17 23:26:23 +0700

    Use request object instead

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,7 +14,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() && Auth::user()->isAdmin()) {
+        if (optional($request->user())->isAdmin()) {
             return $next($request);
         }
 
